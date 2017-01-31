fa415ec24baff8ee8fd676d0d46c669f65249f77
Guido <guido.dechamps@solid-it.biz>
2017-01-31 12:59:15 +0100

    fixed threading

--- src/main/java/be/cooking/Main.java
+++ src/main/java/be/cooking/Main.java
@@ -2,37 +2,49 @@ package be.cooking;
 
 import be.cooking.model.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     private static final int NR_OF_ORDERS_TAKEN = 100;
 
     public static void main(String[] args) {
-        final OrderPrinter orderPrinter = new OrderPrinter();
-        final Cashier cashier = new Cashier(orderPrinter);
-        final Manager manager = new Manager(cashier);
+        final ThreadedHandler orderPrinter = createActor("Printer", new OrderPrinter());
+        final Cashier cashier1 = new Cashier(orderPrinter);
+        final ThreadedHandler cashier = createActor("MoneyMan", cashier1);
+        final ThreadedHandler manager = createActor("Manager", new Manager(cashier));
+        final ThreadedHandler koen = createActor("Cook Koen", new Cook(manager, "Koen"));
+        final ThreadedHandler guido = createActor("Cook Guido", new Cook(manager, "Guido"));
+        final ThreadedHandler greg = createActor("Cook Greg", new Cook(manager, "Greg"));
         final RoundRobin repeater = RoundRobin.newBuilder()
-                .withHandler(new Cook(manager, "Koen"))
-                .withHandler(new Cook(manager, "Guido"))
-                .withHandler(new Cook(manager, "Greg"))
+                .withHandler(koen)
+                .withHandler(guido)
+                .withHandler(greg)
                 .build();
-        final ThreadedHandler threadedHandler = new ThreadedHandler("HandlerBob",repeater);
-        final Waiter waiter = new Waiter(threadedHandler);
+        final ThreadedHandler threadedHandler = new ThreadedHandler("HandlerBob", repeater);
+        final Waiter waiter = new Waiter(repeater);
+        final List<ThreadedHandler> threadedHandlers = Arrays.asList(orderPrinter, cashier, manager, threadedHandler, koen, greg, guido);
 
-        threadedHandler.start();
+        threadedHandlers.forEach(ThreadedHandler::start);
 
         startWorking(waiter);
 
-        waitUntilAllOrdersAreDone(cashier, threadedHandler);
+        waitUntilAllOrdersAreDone(cashier1, threadedHandlers);
         threadedHandler.stop();
 
 
     }
 
-    private static void waitUntilAllOrdersAreDone(Cashier cashier, ThreadedHandler threadedHandler) {
+    private static void waitUntilAllOrdersAreDone(Cashier cashier, List<ThreadedHandler> threadedHandlers) {
         while (cashier.getNrOfOrdersProcessed() != NR_OF_ORDERS_TAKEN) {
-            System.out.println("Queue "+threadedHandler.getName() + " size = " + threadedHandler.size());
+            threadedHandlers.forEach(Main::print);
             Sleep.sleep(200);
         }
+    }
+
+    private static void print(ThreadedHandler threadedHandler) {
+        System.out.println("Queue " + threadedHandler.getName() + " size = " + threadedHandler.size());
     }
 
     private static void startWorking(Waiter waiter) {
@@ -46,6 +58,10 @@ public class Main {
         for (int i = 0; i < NR_OF_ORDERS_TAKEN; i++) {
             waiter.takeOrder(1);
         }
+    }
+
+    private static ThreadedHandler createActor(String name, HandleOrder handler) {
+        return new ThreadedHandler(name, handler);
     }
 
 
